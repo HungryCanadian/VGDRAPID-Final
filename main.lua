1affc01a77c38e90419f99c71391cedd486cd476
HungryCanadian <64567433+HungryCanadian@users.noreply.github.com>
2025-05-05 18:12:03 -0400

    Initial commit

---
+++ main.lua
@@ -0,0 +1,19 @@
+if arg[2] == "debug" then
+    require("lldebugger").start()
+end
+
+function love.draw()
+    love.graphics.print('Hello World!', 400, 300)
+end
+
+print("Test Print!")
+
+local love_errorhandler = love.errorhandler
+
+function love.errorhandler(msg)
+    if lldebugger then
+        error(msg, 2)
+    else
+        return love_errorhandler(msg)
+    end
+end
